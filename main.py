bb492996d3dc97729d9dcc94eb6bc8d94c498e58
PieroMendoza <pieromendoza@upeu.edu.pe>
2021-10-18 12:22:36 -0500

    guardar1

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from routes import api_images
+
+app = Flask(__name__)
+app.register_blueprint(api_images)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host="192.168.1.89")
+    
